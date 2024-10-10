88a90d863be519e9f9e4db7b99076daa136543aa
matthewvarghese1 <mtv5115@psu.edu>
2024-10-10 19:47:22 -0400

    a

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './DataSubmissionForm';
+import './Dashboard.css';
  
 
 const Dashboard = ({ children }) => {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
