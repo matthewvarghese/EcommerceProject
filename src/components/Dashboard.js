d4e83cfa15453cb17c9a15deb2feb218dc1d46aa
matthewvarghese1 <mtv5115@psu.edu>
2024-10-10 19:25:09 -0400

    a

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,26 +1,40 @@
+
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Dashboard.css';
 
-const Dashboard = () => {
+
+const Dashboard = ({ children }) => {
   const navigate = useNavigate();
 
-  const goToHome = () => navigate('/dashboard');
   const goToProfile = () => navigate('/profile');
+  const goToHome = () => navigate('/dashboard');
+  const goToSubmitData = () => navigate('/submit-data');
+  const goToShirts = () => navigate('/shirt');
   const goToLogout = () => navigate('/');
 
   return (
     <div className="dashboard">
       <header className="navbar">
-        <h1>About Me</h1>
+        <h1>Artemis</h1>
         <nav>
           <ul>
             <li><button onClick={goToHome}>Home</button></li>
+            <li><button onClick={goToSubmitData}>Submit Data</button></li>
             <li><button onClick={goToProfile}>Profile</button></li>
             <li><button onClick={goToLogout}>Logout</button></li>
+            <li>
+            </li>
           </ul>
         </nav>
       </header>
+
+      <aside className="sidebar">
+        <h2>Categories</h2>
+        <ul>
+          <li><button onClick={goToShirts}>Products</button></li>
+        </ul>
+      </aside>
 
       <main className="main-content about-me">
         <section className="about-section">
@@ -35,7 +49,7 @@ const Dashboard = () => {
       </main>
 
       <footer className="footer">
-        <p>&copy; 2024 Matthew Varghese. All rights reserved.</p>
+        <p>&copy; 2024 Artemis. All rights reserved.</p>
       </footer>
     </div>
   );
