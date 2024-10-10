c3acb3ce7d99223e9b2ec36254e3ff8f2bb5129d
matthewvarghese1 <mtv5115@psu.edu>
2024-10-10 19:43:41 -0400

    a

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,7 @@
-
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Dashboard.css';
-
+import './DataSubmissionForm';
+ 
 
 const Dashboard = ({ children }) => {
   const navigate = useNavigate();
