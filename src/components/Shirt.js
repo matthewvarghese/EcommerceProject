92536144fd8518752bfb68268d025527f782687f
matthewvarghese1 <mtv5115@psu.edu>
2024-10-11 11:33:08 -0400

    a

--- src/components/Shirt.js
+++ src/components/Shirt.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Dashboard.css';
+import './shirt.css';
 import useProducts from '../components/useProducts';
 import Product from './Product';
 
